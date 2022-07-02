f742ecdf24bea9d141e73cb46e03d6ab903c6941
Shai Yomtov <shaiy202@gmail.com>
2022-07-03 00:33:17 +0300

    added chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [yearFilter, setYearFilter] = useState('All');
@@ -24,6 +25,7 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpensesFilter selected={yearFilter} onFilterYearChange={handleFilterYearChange} />
+      <ExpensesChart expenses={expensesForDisplay} />
       {!expensesForDisplay.length && <p>No expenses to display</p>}
       {!!expensesForDisplay.length &&
         expensesForDisplay.map((expense) => <ExpenseItem key={expense.id} {...expense} />)}
